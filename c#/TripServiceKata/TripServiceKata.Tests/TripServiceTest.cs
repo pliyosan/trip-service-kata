b467f6a6d62115cadd42001dc0aff87ccfe6c07d
Juan Cerezuela <juanjo.cerezuela@asos.com>
2017-07-26 12:02:50 +0100

    first refactor

--- c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
+++ c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
@@ -1,6 +1,57 @@
-﻿namespace TripServiceKata.Tests
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using TripServiceKata.Trip;
+
+namespace TripServiceKata.Tests
 {
+    public class TestableTripService : TripService
+    {
+        private readonly SessionWrapper _sessionWrapper;
+
+        public TestableTripService(SessionWrapper sessionWrapper)
+        {
+            _sessionWrapper = sessionWrapper;
+        }
+        protected override User.User GetLoggedUser()
+        {
+            return _sessionWrapper.GetLoggedUser();
+        }
+    }
+
+    public class SessionWrapper
+    {
+        public User.User GetLoggedUser()
+        {
+            return new User.User();
+        }
+    }
+
+
+    [TestFixture]
     public class TripServiceTest
     {
+        [Test]
+        public void When_Calling_GetTripsByUser_It_BlowsUp_Trying_To_Get_TheLogged_User()
+        {
+            var tripService = new TripService();
+
+            Action getTripsByUserAction = () => tripService.GetTripsByUser(null);
+
+            getTripsByUserAction.ShouldThrow<System.Exception>()
+                .WithMessage("UserSession.GetLoggedUser() should not be called in an unit test");
+        }
+
+        [Test]
+        public void Friend_DoesntHave_Trips_So_It_BlowsUp()
+        {
+            var fakeSessionWrapper = new SessionWrapper();
+            var tripService = new TestableTripService(fakeSessionWrapper);
+
+            Action getTripsByUserAction = () => tripService.GetTripsByUser(null);
+
+            getTripsByUserAction.ShouldThrow<System.Exception>()
+                .WithMessage("UserSession.GetLoggedUser() should not be called in an unit test");
+        }
     }
 }
